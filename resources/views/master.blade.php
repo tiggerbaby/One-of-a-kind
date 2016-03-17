08d2ed0362c9e113e36cb6c8c1eaae9c356f04fd
Alice <alice.wu.nz@gmail.com>
2016-03-17 18:19:32 +1300

    Add sass file. Setting up navigation menu.

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -20,11 +20,29 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
     <body>
-        <li><a href="/">Home</a></li>
-        <li><a href="/story">Our Story</a></li>
-        <li><a href="/work">Our Work</a></li>
-        <li><a href="/shop">Shop</a></li>
-        <li><a href="/contact">Contact Us</a></li>     
+   <nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand hidden-sm hidden-md hidden-lg" href="/"><img src="/img/Current Logo.png" alt="One of A kind logo" id="logo"></a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="/story">Our Story</a></li>
+                <li><a href="/work">Our Work</a></li>
+                <li class="hidden-xs"><a href="/"><img src="/img/Current Logo.png" alt="One of A kind logo" id="logo"></a></li>
+                <li><a href="/shop">Shop Online</a></li>
+                <li><a href="/contact">Contact Us</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
     <div class="container">
    @yield('content')
 
