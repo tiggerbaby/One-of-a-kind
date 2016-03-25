20778e29a548a3ab917b87e16b791c97f28d429e
Alice <alice.wu.nz@gmail.com>
2016-03-26 01:20:23 +1300

    Setting css
    1. Background Img zoom in effect.
    2. Navigation bar hover font color.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,8 +4,9 @@
 @section('meta-description','One of A Kind is a laser cutting desgin company')
 
 @section('content')
- <div class="jumbotron" id="backgroundImg">
-
+ <div class="jumbotron text-center" id="backgroundImg">
+  <h1>One of a kind</h1>
+  <p>offers <strong>New Zealand</strong> made unique and customisable objects.</p>
  
 </div>
  </div>   
