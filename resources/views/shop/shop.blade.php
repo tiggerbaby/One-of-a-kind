95d42078533766036786efe7e69a52906b109095
Alice <alice.wu.nz@gmail.com>
2016-03-28 17:45:27 +1300

    Editing shop page.
    Add content.

--- resources/views/shop/shop.blade.php
+++ resources/views/shop/shop.blade.php
@@ -5,6 +5,43 @@
 
 @section('content')
 <div class="container">
-<h1>Shop</h1>
+ <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+        <h1>Catalogue</h1>
+          <ul class="nav nav-sidebar">
+            <li><a href="#">Catalogue1</a></li>
+		 	<li><a href="#">Catalogue2</a></li>
+		 	<li><a href="#">Catalogue3</a></li>
+		 	<li><a href="#">Catalogue4</a></li>
+		 	<li><a href="#">Catalogue5</a></li>
+		 	<li><a href="#">Catalogue6</a></li>
+          </ul>
+        </div>
+
+<div class="row">
+  <div class="col-sm-4 col-md-4">
+    <div class="thumbnail">
+      <img src="img/sample1.jpg" alt="Sample image">
+      <div class="caption">
+        <h3>Sample 1</h3>
+        <p>$45</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="img/sample2.jpg" alt="Sample image">
+      <div class="caption">
+        <h3>Sample 1</h3>
+        <p>$45</p>
+      </div>
+    </div>
+  </div>
+  
+ 
+</div>
+</div>
+</div>
 </div>
 @endsection
