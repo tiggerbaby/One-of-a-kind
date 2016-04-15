8adcec77168012a378226e89f0f355aefd5fc1b8
Alice <alice.wu.nz@gmail.com>
2016-04-16 00:09:11 +1200

    editing story page

--- resources/views/story/story.blade.php
+++ resources/views/story/story.blade.php
@@ -20,25 +20,29 @@
   </div>
 </div>
 </div>
-
-<hr>
-<div class="row">
-  <div class="storyBox">
-  <div class="col-md-7 col-md-push-6">
-    <h1 class="text-center">Our Story</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur.</p>
-  </div>
-  <div class="col-md-5 col-md-pull-6">
-      <img src="/img/rings.jpg" alt="Key ring image" class="img-responsive center-block">
-  </div>
-</div>
 </div>
 
 <hr>
+<div class="container">
+  <div class="row">
+    <div class="storyBox">
+    <div class="col-md-7 col-md-push-6">
+      <h1 class="text-center">Our Story</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur.</p>
+    </div>
+    <div class="col-md-5 col-md-pull-6">
+        <img src="/img/rings.jpg" alt="Key ring image" class="img-responsive center-block">
+    </div>
+  </div>
+  </div>
+</div>
+
+<hr>
+<div class="container">
 <div class="row">
   <div class="storyBox">
   <div class="col-md-7">
@@ -52,10 +56,9 @@
   <div class="col-md-5">
       <img src="/img/sample4.jpg" alt="Coasters image" class="img-responsive center-block"> 
   </div>
+</div>   
+</div>
 </div>
 
-     
-</div>
-</div>
 
 @endsection
