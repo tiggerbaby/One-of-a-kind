5c3aa9fd5c63270ba0a4c243bd6b7db301bbc9e2
Alice <alice.wu.nz@gmail.com>
2016-04-23 21:03:50 +1200

    Add view single product template

--- resources/views/shop/view.blade.php
+++ resources/views/shop/view.blade.php
@@ -4,7 +4,33 @@
 @section('meta-description','View Object')
 
 @section('content')
-
+<div class="container">
+@if(\Auth::check())
+    @if(\Auth::user()->role === 'admin')
+    <button type="submit" class="btn btn-default"><a href="/shop/view/{{$singleProduct->id}}/edit"><span class="glyphicon glyphicon-pencil"></span>Edit Object</a></button>
+     <button type="submit" class="btn btn-default" data-toggle="modal" data-target="#exampleModal"><span class="glyphicon glyphicon-trash"></span>Delete Object</button>
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="exampleModalLabel">Are you sure?</h4>
+      </div>
+      <div class="modal-body">
+        <p>You are about to <em>permanently</em> delete this object.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-default"><a href="/shop/view/{{ $singleProduct->id }}/delete_confirm"><span class="glyphicon glyphicon-trash"></span>Delete object</a></button>
+      </div>
+    </div>
+  </div>
+</div>
+    
+    @endif()
+   @endif()
+</div>
+<br>
 <div class="container" id="objectPage">
 	<div class="row">
 	  <div class="col-md-5">
